1a259116712d44233bb2d69f8521090853b28e7a
Besim <Besim.musliu@student.kyh.se>
2020-10-02 16:51:08 +0200

    Filled out GetTollFeePerPassing method

--- src/com/company/TollFeeCalculator_v2Test.java
+++ src/com/company/TollFeeCalculator_v2Test.java
@@ -14,7 +14,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Testing TollFeeCalculator")
 public class TollFeeCalculator_v2Test {
-    private final TollFeeCalculator_v2 tollfeeTest = new TollFeeCalculator_v2("testData/Lab4.txt");
     @BeforeEach
     void before() {
         System.out.println("Test starting");
@@ -42,15 +41,23 @@ public class TollFeeCalculator_v2Test {
     @DisplayName("Testing getTollFeePerPassing")
     void getTollFeePerPassing() {
         LocalDateTime date = LocalDateTime.parse("2020-06-30 00:05", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-        assertEquals(0, );
+        assertEquals(0, TollFeeCalculator_v2.getTollFeePerPassing(date));
         LocalDateTime date2 = LocalDateTime.parse("2020-06-30 06:34", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        assertEquals(13, TollFeeCalculator_v2.getTollFeePerPassing(date2));
         LocalDateTime date3 = LocalDateTime.parse("2020-06-30 08:52", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        assertEquals(8, TollFeeCalculator_v2.getTollFeePerPassing(date3));
         LocalDateTime date4 = LocalDateTime.parse("2020-06-30 10:13", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        assertEquals(8, TollFeeCalculator_v2.getTollFeePerPassing(date4));
         LocalDateTime date5 = LocalDateTime.parse("2020-06-30 10:25", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        assertEquals(8, TollFeeCalculator_v2.getTollFeePerPassing(date5));
         LocalDateTime date6 = LocalDateTime.parse("2020-06-30 11:04", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        assertEquals(8, TollFeeCalculator_v2.getTollFeePerPassing(date6));
         LocalDateTime date7 = LocalDateTime.parse("2020-06-30 16:50", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        assertEquals(18, TollFeeCalculator_v2.getTollFeePerPassing(date7));
         LocalDateTime date8 = LocalDateTime.parse("2020-06-30 18:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        assertEquals(8, TollFeeCalculator_v2.getTollFeePerPassing(date8));
         LocalDateTime date9 = LocalDateTime.parse("2020-06-30 21:30", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        assertEquals(0, TollFeeCalculator_v2.getTollFeePerPassing(date9));
         LocalDateTime date10 = LocalDateTime.parse("2020-07-01 00:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
 
 
